5450d64deabaf83e257b1ffad11e5e2f7381a4a6
B3xis <atatanishe@gmail.com>
2020-10-08 13:59:34 +0300

    New_Window
    a

---
+++ TestChanger.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+
+namespace exam_test
+{
+    /// <summary>
+    /// Interaction logic for TestChanger.xaml
+    /// </summary>
+    public partial class TestChanger : Window
+    {
+        public TestChanger()
+        {
+
+            InCharger inCharger = new InCharger();
+
+
+            InitializeComponent();
+
+
+        }
+
+        public class InCharger
+        {
+
+        }
+
+    }
+}
