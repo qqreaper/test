388524446ed2db2c0ac649eeb91999405f857a0c
MrBaster <45029393+MrBaster@users.noreply.github.com>
2020-10-08 13:19:33 +0300

    1st try

---
+++ Test.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace exam_test
+{
+    /// <summary>
+    /// Interaction logic for Test.xaml
+    /// </summary>
+    public partial class Test : Window
+    {
+        public Test()
+        {
+            TestTestClass test = new TestTestClass();
+
+            questionField.Content = test.question;
+            InitializeComponent();
+        }
+
+        public class TestTestClass
+        {
+            public string question = "qweqwe";
+            public int qquant = 3;
+            public int rightQnum = 2;
+        }
+    }
+}
