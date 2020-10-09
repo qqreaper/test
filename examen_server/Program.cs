11752f7dd33c5bd3ddba76b31e768d428b392d2c
QQ <44963782+qqreaper@users.noreply.github.com>
2020-10-09 18:33:08 +0300

    Server update

--- examen_server/Program.cs
+++ examen_server/Program.cs
@@ -20,6 +20,20 @@ namespace examen_server
     {
         const int port = 8888; 
         Logger logger = new Logger();
+        public void GetInfo() { 
+        //обращение к бд которое не запилил калинин
+        //return(data);
+        }
+        public void SetInfo(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            //обращение к бд которое не запилил калинин
+            string response = /*результат из бд*/ "";
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            stream.Write(data, 0, data.Length);
+            logger.Log($"Отправлены данные клиенту: {response}");
+            stream.Close();
+        }
         public void StartServer()
         {
             TcpListener server = null;
@@ -34,14 +48,8 @@ namespace examen_server
                 {
                     logger.Log("Ожидание подключений... ");
                     TcpClient client = server.AcceptTcpClient();
-                    logger.Log("Подключен клиент. Выполнение запроса...");
-                    NetworkStream stream = client.GetStream();
-                    string response = "тест";
-                    byte[] data = Encoding.UTF8.GetBytes(response);
-                    stream.Write(data, 0, data.Length);
-                    logger.Log($"Отправлено сообщение: {response}");
-                    stream.Close();
-                    client.Close();
+                    logger.Log("Подключен клиент.");
+                    
                 }
             }
             catch (Exception e)
