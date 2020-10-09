11f7975f710af2cd305ba54aa2d43aedbe2b6780
MrBaster <45029393+MrBaster@users.noreply.github.com>
2020-10-09 13:38:11 +0300

    смысл нормално комментировать, если мастер не работает?

--- Test.xaml.cs
+++ Test.xaml.cs
@@ -27,37 +27,64 @@ namespace exam_test
 
             questionField.Content = answerObj.question;
 
-            AddAnsvers(answerObj, this);
+            AddAnsversRadio(answerObj, this);
+
+
         }
-        private void AddAnsvers(AnswerClass answerObj, Test test)
+        private void AddAnsversRadio(AnswerClass answerObj, Test test)
         {
             int TopMargin = 0;
+            int boxName = 1;
             //test
             answerObj.answers = new List<string>();
             answerObj.answers.Add("asd");
             answerObj.answers.Add("qwe");
             answerObj.answers.Add("zxc");
+
+
             //test
+            int i = 0;
+
+            if (answerObj.isRadio)
+                RadioButton[] radioButtons = new RadioButton[answerObj.questionQuant];
+            else
+                CheckBox[] chekBoxes = new CheckBox[answerObj.questionQuant];
 
             foreach (string item in answerObj.answers)
             {
-                if(answerObj.isRadio)
-                test.AnsversGrid.Children.Add(new RadioButton() { Content = item, Margin = new Thickness(10, TopMargin, 10, 10) });
+
+                if (answerObj.isRadio)
+                {
+                    radioButtons[i] = new RadioButton() { Name = "box" + Convert.ToString(boxName), Content = item, Margin = new Thickness(10, TopMargin, 10, 10) };
+                    test.AnsversGrid.Children.Add(radioButtons[i]);
+                }
                 else
-                    test.AnsversGrid.Children.Add(new CheckBox() { Content = item, Margin = new Thickness(10, TopMargin, 10, 10) });
+                {
+                    chekBoxes[i] = new RadioButton() { Name = "box" + Convert.ToString(boxName), Content = item, Margin = new Thickness(10, TopMargin, 10, 10) };
+                    test.AnsversGrid.Children.Add(chekBoxes[i]);
+                }
 
 
+                //foreach (var item in test.AnsversGrid.Children.)
+                //{
+                //    if (item.Name == "box" + Convert.ToString(1)) ;
+
+                i++;
                 TopMargin += 20;
+                boxName++;
             }
+
+
         }
-        public class AnswerClass
-        {
-            public bool isRadio = true;
-            public string question = "qweqwe";
-            public List<string> answers;
-            public int rightQnum = 2;
-            public List<int> rightAnsvers;
-           
-        }
+    }
+    public class AnswerClass
+    {
+        public bool isRadio = true;
+        public string question = "qweqwe";
+        public List<string> answers;
+        public int rightQnum = 2;
+        public List<int> rightAnsvers;
+        public int questionQuant = 3;
+
     }
 }
