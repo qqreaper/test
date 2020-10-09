7e6ab724b8948b939029ae5b2d23e3cec85e8d3b
kdaniilm <kdaniilm@gmail.com>
2020-10-09 18:18:40 +0300

    Create class for work with db

--- examen_server/ConsoleApp7/Program.cs
+++ examen_server/ConsoleApp7/Program.cs
@@ -190,7 +190,147 @@ namespace ConsoleApp7
         public DbSet<Question> Questions { get; set; }
         public DbSet<Ansver> Ansvers { get; set; }
     }
-
+    public class DataBaseWork
+    {
+        private readonly Lazy<DataBaseWork> dataBaseWork = new Lazy<DataBaseWork>(() => new DataBaseWork());
+        public DataBaseWork() { }
+        #region Work with Users
+        public void AddToUsersDb(string login, string passHash, int compliteTests)
+        {
+            lock (dataBaseWork)
+            {
+                using(ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    applicationContext.Users.Add(new User { Login = login, PassHash = passHash, CompliteTests = compliteTests });
+                    applicationContext.SaveChangesAsync();
+                    Console.WriteLine($"User {login} {passHash} added");
+                }
+            }
+        }
+        public void RemoveFromUsersDb(string login, string passHash)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    if (applicationContext.Users.ToList().Where(x => x.Login == login && x.PassHash == passHash).Count() > 0)
+                    {
+                        User userForRemove = applicationContext.Users.ToList().Where(x => x.Login == login && x.PassHash == passHash).Last();
+                        applicationContext.Users.Remove(userForRemove);
+                        applicationContext.SaveChangesAsync();
+                        Console.WriteLine($"User {login} {passHash} deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("404(User not found)");
+                    }
+                }
+            }
+        }
+        #endregion
+        #region Work with Test
+        public void AddToTestsDb(string testName)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    applicationContext.Tests.Add(new Test { TestName = testName});
+                    applicationContext.SaveChangesAsync();
+                    Console.WriteLine($"Test {testName} added");
+                }
+            }
+        }
+        public void RemoveFromTestsDb(string testName)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    if (applicationContext.Tests.ToList().Where(x => x.TestName == testName).Count() > 0)
+                    {
+                        Test testForRemove = applicationContext.Tests.ToList().Where(x => x.TestName == testName).Last();
+                        applicationContext.Tests.Remove(testForRemove);
+                        applicationContext.SaveChangesAsync();
+                        Console.WriteLine($"Test {testName} deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("404(Test not found)");
+                    }
+                }
+            }
+        }
+        #endregion
+        #region Work with Questions
+        public void AddToQuestionsDb(string questionText, int testsId)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    applicationContext.Questions.Add(new Question { QuestionText = questionText, TestsId = testsId });
+                    applicationContext.SaveChangesAsync();
+                    Console.WriteLine($"Question {questionText} {testsId} added");
+                }
+            }
+        }
+        public void RemoveFromQuestionsDb(string questionText, int testsId)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    if (applicationContext.Questions.ToList().Where(x => x.QuestionText == questionText && x.TestsId == testsId).Count() > 0)
+                    {
+                        Question questionForRemove = applicationContext.Questions.ToList().Where(x => x.QuestionText == questionText && x.TestsId == testsId).Last();
+                        applicationContext.Questions.Remove(questionForRemove);
+                        applicationContext.SaveChangesAsync();
+                        Console.WriteLine($"Question {questionText} deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("404(Question not found)");
+                    }
+                }
+            }
+        }
+        #endregion
+        #region Work with Answer
+        public void AddToAnswersDb(string ansverText, int isRight, int questionId)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    applicationContext.Ansvers.Add(new Ansver { AnsverText = ansverText, IsRight = isRight, QuestionsId = questionId});
+                    applicationContext.SaveChangesAsync();
+                    Console.WriteLine($"Answer {ansverText} {isRight} added");
+                }
+            }
+        }
+        public void RemoveFromAnswersDb(string ansverText, int isRight, int questionId)
+        {
+            lock (dataBaseWork)
+            {
+                using (ApplicationContext applicationContext = new ApplicationContext())
+                {
+                    if (applicationContext.Ansvers.ToList().Where(x => x.AnsverText == ansverText && x.IsRight == isRight && x.QuestionsId == questionId).Count() > 0)
+                    {
+                        Ansver answerForRemove = applicationContext.Ansvers.ToList().Where(x => x.AnsverText == ansverText && x.IsRight == isRight && x.QuestionsId == questionId).Last();
+                        applicationContext.Ansvers.Remove(answerForRemove);
+                        applicationContext.SaveChangesAsync();
+                        Console.WriteLine($"Ansver {ansverText} deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("404(Ansver not found)");
+                    }
+                }
+            }
+        }
+        #endregion
+    }
     class Program
     {
         private static string Ip = "127.0.0.1";
@@ -199,23 +339,7 @@ namespace ConsoleApp7
         {
             using(ApplicationContext applicationContext = new ApplicationContext())
             {
-                applicationContext.Users.Add(new User { Login = "asd", PassHash = "asd".GetHashCode().ToString(), CompliteTests = 0 });
-                applicationContext.SaveChanges();
-                applicationContext.Tests.Add(new Test { TestName = "test"});
-                applicationContext.SaveChanges();
-                applicationContext.Questions.Add(new Question { QuestionText = "question", TestsId = 1});
-                applicationContext.SaveChanges();
-                applicationContext.Ansvers.Add(new Ansver { AnsverText = "ansver1", IsRight = 1, QuestionsId = 1 });
-                applicationContext.SaveChanges();
-                Console.WriteLine(string.Format("{0} {1} {2}", applicationContext.Users.ToList().Where(x => x.Id == 1).Last().Login, applicationContext.Users.ToList().Where(x => x.Id == 1).Last().PassHash,
-                    applicationContext.Users.ToList().Where(x => x.Id == 1).Last().CompliteTests.ToString()));
-                Console.WriteLine(applicationContext.Tests.ToList().Where(x => x.Id == 1).Last().TestName);
-                Console.WriteLine(string.Format("{0} {1}", applicationContext.Questions.ToList().Where(x => x.Id == 1).Last().QuestionText,
-                    applicationContext.Questions.ToList().Where(x => x.Id == 1).Last().TestsId.ToString()));
-                Console.WriteLine(string.Format("{0} {1} {2}", applicationContext.Ansvers.ToList().Where(x => x.Id == 1).Last().AnsverText,
-                    applicationContext.Ansvers.ToList().Where(x => x.Id == 1).Last().IsRight.ToString(),
-                    applicationContext.Ansvers.ToList().Where(x => x.Id == 1).Last().QuestionsId.ToString()));
-                Console.WriteLine("Ok");
+                
             }
 
             TemplateServer template = new TemplateServer();
